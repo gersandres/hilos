688f882801fa0e42dbf4fdbbee087e3ac4ff418b
gersandres <gersandres@gmail.com>
2016-04-21 19:10:28 -0500

    mi p

---
+++ Hilos1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Hilos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread hilo = new Thread(pintarCaracter);
+            hilo.Start();
+
+            Thread hilo2 = new Thread(()=> pintarPalabra(".NET","C#"));
+            hilo2.Start();
+            
+           // hilo.Join();
+            //Thread.Sleep(2000);
+            //hilo.Start("z");
+            pintarX();
+            Console.Read();
+        }
+        static void pintarCaracter(Object letra)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(letra.ToString());
+
+            }
+        }
+
+        //static void pintarCaracter()
+        //{
+        //    for (int i = 0; i < 1000; i++)
+        //    {
+        //        Console.Write("y");
+        //    }
+        //}
+
+        static void pintarPalabra(String palabra1, String palabra2)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(palabra1 + "-" + palabra2);
+            }
+        }
+
+
+        static void pintarX()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("x");
+
+            }
+        }
+
+    }
+}
